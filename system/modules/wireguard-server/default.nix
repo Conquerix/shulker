63de0d1385f39c558c13d691f326100a21484578
Shulker ROOT <root@shulker.fr>
2022-12-05 22:40:27 +0100

    corrected wireguard-server module

--- system/modules/wireguard-server/default.nix
+++ system/modules/wireguard-server/default.nix
@@ -34,20 +34,16 @@ in
         # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
         # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
         postSetup = ''
-          ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 192.168.10.0/24 -o eth0 -j MASQUERADE
+          ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 192.168.10.0/24 -o cfg.extInterface -j MASQUERADE
         '';
   
         # This undoes the above command
         postShutdown = ''
-          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 192.168.10.0/24 -o eth0 -j MASQUERADE
+          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 192.168.10.0/24 -o cfg.extInterface -j MASQUERADE
         '';
   
         # Path to the private key file.
-        #
-        # Note: The private key can also be included inline via the privateKey option,
-        # but this makes the private key world-readable; thus, using privateKeyFile is
-        # recommended.
-        privateKeyFile = ;
+        privateKeyFile = "/etc/wireguard/private_key";
   
         peers = [
           # List of allowed peers.
