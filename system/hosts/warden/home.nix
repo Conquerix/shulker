bdd521349cf9fa6883127f1ac6832e94b1d8d3af
Pierre Fournier <pierre@fournier.net>
2022-12-09 07:23:15 +0100

    cleanup + switching warden to common flake

--- system/hosts/warden/home.nix
+++ system/hosts/warden/home.nix
@@ -6,24 +6,13 @@
 
   shulker = {
     modules = {
-      app = {
-        discord.enable = true;
-      };
-      dev = {
-        cc.enable = true;
-        nix.enable = true;
-        python.enable = true;
-      };
       shell = {
-        direnv.enable = true;
         ssh.enable = true;
-        zsh.enable = true;
       };
     };
 
     profiles = {
       common.enable = true;
-      development.enable = true;
     };
   };
 }
