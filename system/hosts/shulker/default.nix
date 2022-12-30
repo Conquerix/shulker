90c6a3e70571e64b67008699d5709784debfb189
Shulker ROOT <root@shulker.fr>
2022-12-30 10:55:06 +0100

    added velocity minecraft proxy

--- system/hosts/shulker/default.nix
+++ system/hosts/shulker/default.nix
@@ -1,28 +1,14 @@
 { ... }:
 
 {
-  imports = [ ./hardware.nix ];
+  imports = [ 
+    ./hardware.nix 
+    ./minecraft.nix
+  ];
 
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 80 443 23231 25501 ];
-    allowedUDPPorts = [ 25501 ];
-  };
-
-  security.acme.acceptTerms = true;
-
-  services.nginx = {
-    enable = true;
-    streamConfig = ''
-      upstream warden-minecraft-VH2 {
-        server 192.168.10.2:25501;
-      }
-      
-      server {
-        listen 25501;
-        proxy_pass warden-minecraft-VH2;
-      }
-    '';
+    allowedTCPPorts = [ 80 443 ];
   };
 
   shulker = {
@@ -30,6 +16,7 @@
       user.home = ./home.nix;
       ssh_server.enable = true;
       soft-serve.enable = true;
+      docker.enable = true;
       wireguard.server = {
       	enable = true;
       	extInterface = "ens3";
