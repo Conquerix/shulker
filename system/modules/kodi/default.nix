1b2daa4f0722b1a725d04b4175afcb41db224060
Guardian root <pierre@fournier.net>
2023-05-31 20:55:11 +0200

    Added dummy kodi

---
+++ system/modules/kodi/default.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+let cfg = config.shulker.modules.kodi;
+in
+{
+  options.shulker.modules.kodi = {
+      enable = mkEnableOption "Enable Kodi";
+    };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs.gnomeExtensions; [
+   	  kodi
+    ];
+}
