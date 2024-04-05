9576bb2c59333d8c0978a1aacf225822be11fde2
Pierre Fournier <pierre@fournier.net>
2024-04-05 19:45:45 +0200

    continuation

--- home/modules/app/discord/default.nix
+++ home/modules/app/discord/default.nix
@@ -11,7 +11,7 @@ in
       [
         # This is required to be from unstable as discord will sometimes soft-lock
         # on "there is an update" screen.
-        discord
+        discord-canary
       ];
   };
 }
