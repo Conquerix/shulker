3ed35d4b8d95a75ddbe77e91ae7362d2fb1e9f32
Pierre Fournier <pierre@fournier.net>
2024-04-14 18:56:23 +0200

    changed wireguard again because wgautomesh doesn't work

--- system/modules/wireguard/default.nix
+++ system/modules/wireguard/default.nix
@@ -4,82 +4,102 @@ with lib;
 let 
   cfg = config.shulker.modules.wireguard;
   wgPort = 51821;
-  wgHostInfo = {
-    shulker = {
+  devices = [
+    {
+      name = "shulker";
       address = "10.10.10.1";
-      pubkey = "vLo4XYe84WcCnkLynjO2SjBzHmFuYeuFN0CF5b/CfBc=";
-      endpoint = "51.178.27.137:${toString wgPort}";
-    };
-    warden = {
+      publicKey = "vLo4XYe84WcCnkLynjO2SjBzHmFuYeuFN0CF5b/CfBc=";
+      endpoint = {
+        ip = "51.178.27.137";
+        port = wgPort;
+      };
+    }
+    {
+      name = "warden";
       address = "10.10.10.2";
-      pubkey = "gfwhPNVOMoBxaCXP+dIcuLv+r2aat127wE+vO20Y/l0=";
-    };
-    phantom = {
+      publicKey = "gfwhPNVOMoBxaCXP+dIcuLv+r2aat127wE+vO20Y/l0=";
+    }
+    {
+      name = "phantom";
       address = "10.10.10.3";
-      pubkey = "f2m5/gMPILz9ISGiAtwZFEWcU01jzU899bJBuhTiuyc=";
-    };
-    wither = {
+      publicKey = "f2m5/gMPILz9ISGiAtwZFEWcU01jzU899bJBuhTiuyc=";
+    }
+    {
+      name = "wither";
       address = "10.10.10.4";
-      pubkey = "9JNyydvMlIDGpOCwAW6a8TzPALGvRq2UKBiPFLV8GwM=";
-    };
-    guardian = {
+      publicKey = "9JNyydvMlIDGpOCwAW6a8TzPALGvRq2UKBiPFLV8GwM=";
+    }
+    {
+      name = "guardian";
       address = "10.10.10.5";
-      pubkey = "J2amctRH90iC1bnd2UnqOp9D9Rpbgmb0w/Xs+caB83U=";
-    };
-    vindicator = {
+      publicKey = "J2amctRH90iC1bnd2UnqOp9D9Rpbgmb0w/Xs+caB83U=";
+    }
+    {
+      name = "vindicator";
       address = "10.10.10.6";
-      pubkey = "2xrdv1hBlJAQx8jo5P6hie6QzWSjbdGC8wP4pvCP6Rs=";
-      endpoint = "141.94.96.139:${toString wgPort}";
-    };
-    enderdragon = {
+      publicKey = "2xrdv1hBlJAQx8jo5P6hie6QzWSjbdGC8wP4pvCP6Rs=";
+      endpoint = {
+        ip = "141.94.96.139";
+        port = wgPort;
+      };
+    }
+    {
+      name = "enderdragon";
       address = "10.10.10.7";
-      pubkey = "k40E/7Z1DpaiwkTPTnn660N7A/V9jwgwjsL2Lm0OSlU=";
-      endpoint = "136.243.40.242:${toString wgPort}";
-    };
-    endermite = {
+      publicKey = "k40E/7Z1DpaiwkTPTnn660N7A/V9jwgwjsL2Lm0OSlU=";
+      endpoint = {
+        ip = "136.243.40.242";
+        port = wgPort;
+      };
+    }
+    {
+      name = "endermite";
       address = "10.10.10.8";
-      pubkey = "LoGAYxqvBJYxBOY39VRd9vmyVAy592NDxQb2iBOf5wU=";
-    };
-  };
+      publicKey = "LoGAYxqvBJYxBOY39VRd9vmyVAy592NDxQb2iBOf5wU=";
+    }
+  ];
 
-  host = wgHostInfo.${config.networking.hostName};
-in
-{
+  hosts = listToAttrs (map (x: {name = x.name; value = x;}) devices);
+
+  host = hosts.${config.networking.hostName};
+
+in {
+
   options.shulker.modules.wireguard = {
     enable = mkEnableOption "Enable wireguard server";
   };
 
   config = mkIf cfg.enable {
-    networking.firewall = {
-      allowedTCPPorts = [ wgPort ];
-      allowedUDPPorts = [ wgPort ];
-      trustedInterfaces = ["wg-shulker"];
-    };
-  
-    networking.wireguard.interfaces.wg-shulker = {
-      ips = ["${host.address}/24"];
-      privateKeyFile = "/secrets/wireguard-private-key";
-    };
+    networking = {
+      # Map the hostnames for easy addressing, both as "hostname" and "hostname.wg"
+      hosts = listToAttrs (map (x: {
+        name = x.address;
+        value = [ "${x.name}" "${x.name}.wg" ];
+      }) devices);
+      firewall = {
+        allowedUDPPorts = [ wgPort ];
+        # Trust all traffic on this interface
+        trustedInterfaces = [ "wg-shulker" ];
+      };
+      wireguard.interfaces.wg-shulker = {
+        privateKeyFile = "/secrets/wireguard-private-key";
+        ips = [ "${host.address}/24" ];
+        listenPort = wgPort;
 
-    services.wgautomesh = {
-      enable = true;
-      gossipSecretFile = "/secrets/wgautomesh-gossip-secret";
-      enablePersistence = true;
-      settings = {
-        interface = "wg-shulker";
-        peers = pkgs.lib.mapAttrsToList (_: value: value) (
-          removeAttrs wgHostInfo [config.networking.hostName]
-        );
+        # There is no problem with having a peer device listed for itself, it is ignored.
+        # Endpoints are a hint as to were to find the device, but connections can be accepted from anywhere.
+
+        # Maps the devices list to configured Wireguard peers.
+        # It adds an endpoint if that device has a public endpoint, which
+        # should be the case for all servers.
+        peers = map (x: {
+          inherit (x) publicKey;
+          allowedIPs = [ "${x.address}/32" ];
+          endpoint = mkIf (x ? endpoint) "${x.endpoint.ip}:${toString x.endpoint.port}";
+        }) devices;
       };
     };
 
-    environment.persistence."/nix/persist" = mkIf (config.shulker.modules.impermanence.enable) {
-      files = [
-        {file = "/var/lib/wgautomesh/state"; parentDirectory = { mode = "u=rw,g=,o="; };}
-      ];
-    };
-
     opsm.secrets.wireguard-private-key.secretRef = "op://Shulker/${config.networking.hostName}/Wireguard Private Key";
-    opsm.secrets.wgautomesh-gossip-secret.secretRef = "op://Shulker/wgautomesh/secret key";
   };
 }
