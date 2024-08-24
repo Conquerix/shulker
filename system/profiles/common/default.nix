18594530a01022e6912f5cf69b1e25823dec6687
Pierre Fournier <pierre@fournier.net>
2024-08-24 14:37:48 +0200

    disable network manager wait online network wide

--- system/profiles/common/default.nix
+++ system/profiles/common/default.nix
@@ -23,6 +23,10 @@ with lib;
       dns = "none";
     };
     networking.nameservers = [ "9.9.9.9" ];
+
+    # Disable this to try and solve the nework manager wait online failed after each rebuild.
+    systemd.network.wait-online.enable = false;
+    boot.initrd.systemd.network.wait-online.enable = false;
     
 
     programs.ssh.extraConfig = "IdentityFile /secrets/ssh-ed25519-key";
