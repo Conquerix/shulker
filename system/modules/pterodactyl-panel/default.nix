415a16a17538f862f614748ee4503126c8016d98
Pierre Fournier <pierre@fournier.net>
2024-11-03 21:57:47 +0100

    update pterodactyl panel nginx entry

--- system/modules/pterodactyl-panel/default.nix
+++ system/modules/pterodactyl-panel/default.nix
@@ -87,15 +87,15 @@ in
         useACMEHost = cfg.baseUrl;
         locations."/" = {
           proxyPass = "http://127.0.0.1:${toString cfg.port}";
-          extraConfig = ''
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header Host $host;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_redirect off;
-            proxy_buffering off;
-            proxy_request_buffering off;
-          '';
+          #extraConfig = ''
+          #  proxy_set_header X-Real-IP $remote_addr;
+          #  proxy_set_header Host $host;
+          #  proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          #  proxy_set_header X-Forwarded-Proto $scheme;
+          #  proxy_redirect off;
+          #  proxy_buffering off;
+          #  proxy_request_buffering off;
+          #'';
         };
       };
     };
@@ -173,6 +173,7 @@ in
         QUEUE_DRIVER=redis
         REDIS_HOST=cache
         SESSION_DRIVER=redis
+        TRUSTED_PROXIES=127.0.0.1
       '';
       mode = "0600";
     };
