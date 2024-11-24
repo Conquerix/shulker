9731a1c10835ca02cff77ffd77e9be536bae99b7
Pierre Fournier <pierre@fournier.net>
2024-11-24 18:43:31 +0100

    add tailscale

---
+++ system/modules/tailscale/default.nix
@@ -0,0 +1,25 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+let
+  cfg = config.shulker.modules.tailscale;
+in
+{
+  options.shulker.modules.tailscale = {
+    enable = mkEnableOption "Enable tailscale client";
+  };
+
+  config = mkIf cfg.enable {
+    services.tailscale = {
+      enable = true;
+      authKeyFile = config.opnix.secrets.tailscale-auth-key.path;
+      extraUpFlags = [
+        "--login-server https://vpn.shulker.link"
+      ];
+    };
+
+    opnix.secrets.tailscale-auth-key = {
+      source = "{{ op://Shulker/Headscale Preauth Key/key }}";
+    };
+  };
+}
