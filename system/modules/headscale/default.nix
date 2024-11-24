96b7e8416a66f8c5e7ff0520bdb8659efa6a7337
Pierre Fournier <pierre@fournier.net>
2024-11-24 16:03:00 +0100

    fix headscale nginx entry

--- system/modules/headscale/default.nix
+++ system/modules/headscale/default.nix
@@ -67,18 +67,8 @@ in
         useACMEHost = cfg.baseUrl;
         locations."/" = {
           proxyPass = "http://127.0.0.1:${toString cfg.port}";
+          proxyWebsockets = true;
         };
-        extraConfig = ''
-          proxy_http_version 1.1;
-          proxy_set_header Upgrade $http_upgrade;
-          proxy_set_header Connection $connection_upgrade;
-          proxy_set_header Host $server_name;
-          proxy_buffering off;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
-          add_header Strict-Transport-Security "max-age=15552000; includeSubDomains" always;
-        '';
       };
     };
 
