389f78be38067923cc0aec6e63dae141e705588a
Pierre Fournier <pierre@fournier.net>
2024-11-29 19:20:47 +0100

    disable stub dns on systemd-resolved

--- system/profiles/common/default.nix
+++ system/profiles/common/default.nix
@@ -49,6 +49,9 @@ with lib;
       domains = [ "~." ];
       fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
       dnsovertls = "true";
+      extraConfig = ''
+        DNSStubListener=no
+      '';
     };
 
     security.sudo.enable = false;
