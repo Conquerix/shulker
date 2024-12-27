8223f888e4eb88b8e1d7d4e65c30ffacc54e5e43
Pierre Fournier <pierre@fournier.net>
2024-12-27 17:41:55 +0100

    add sshfs to seedbox

--- system/hosts/warden/default.nix
+++ system/hosts/warden/default.nix
@@ -25,6 +25,20 @@
     };
   };
 
+  fileSystems."/remote/torrents" = {
+    device = "conquerix@spinel.usbx.me:/home/conquerix/downloads/rtorrent";
+    fsType = "fuse.sshfs";
+    options = [
+      "identityfile=${config.opnix.secrets.ssh-ed25519-host-key.path}"
+      "idmap=user"
+      "x-systemd.automount" # mount the filesystem automatically on first access
+      "allow_other" # don't restrict access to only the user which `mount`s it (because that's probably systemd who mounts it, not you)
+      "user" # allow manual `mount`ing, as ordinary user.
+      "_netdev"
+    ];
+  };
+  boot.supportedFilesystems."fuse.sshfs" = true;
+
   shulker = {
     profiles.server.enable = true;
     modules = {
