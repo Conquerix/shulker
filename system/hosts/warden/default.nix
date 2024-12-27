611e20de6d2187b45a9af6917c9c638510dca6ce
Pierre Fournier <pierre@fournier.net>
2024-12-27 22:36:51 +0100

    add second remote path to seedbox

--- system/hosts/warden/default.nix
+++ system/hosts/warden/default.nix
@@ -37,6 +37,18 @@
       "_netdev"
     ];
   };
+  fileSystems."/remote/usenet" = {
+    device = "conquerix@spinel.usbx.me:/home/conquerix/downloads/sabnzbd/complete";
+    fsType = "fuse.sshfs";
+    options = [
+      "identityfile=${config.opnix.secrets.ssh-ed25519-host-key.path}"
+      "idmap=user"
+      "x-systemd.automount" # mount the filesystem automatically on first access
+      "allow_other" # don't restrict access to only the user which `mount`s it (because that's probably systemd who mounts it, not you)
+      "user" # allow manual `mount`ing, as ordinary user.
+      "_netdev"
+    ];
+  };
   boot.supportedFilesystems."fuse.sshfs" = true;
 
   shulker = {
