aad42f846a6d458e79df836b915b510f2c075fc8
Pierre Fournier <pierre@fournier.net>
2025-01-01 16:26:36 +0100

    Added intel a380 gpu in warden

--- system/hosts/warden/default.nix
+++ system/hosts/warden/default.nix
@@ -51,6 +51,22 @@
   };
   boot.supportedFilesystems."fuse.sshfs" = true;
 
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-vaapi-driver # previously vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
+      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+      vpl-gpu-rt # QSV on 11th gen or newer
+      intel-media-sdk # QSV up to 11th gen
+    ];
+  };
+
   shulker = {
     profiles.server.enable = true;
     modules = {
