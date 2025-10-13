ba0695ee714fa8b65085ba5d20722b8e7faf5127
conquerix <conquerix@shulker.link>
2025-10-13 23:23:51 +0200

    fix affine nginx proxy pass

--- system/modules/affine/default.nix
+++ system/modules/affine/default.nix
@@ -169,7 +169,7 @@ in
         useACMEHost = cfg.baseUrl;
         locations."/" = {
           proxyWebsockets = true;
-          proxyPass = "https://127.0.0.1:${toString cfg.port}";
+          proxyPass = "http://127.0.0.1:${toString cfg.port}";
         };
       };
     };
