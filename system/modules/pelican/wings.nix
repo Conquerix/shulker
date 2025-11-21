bed6f24e98a0d7e6a9080c26d125b05ca695d75b
conquerix <conquerix@shulker.link>
2025-11-21 23:04:29 +0100

    typo

--- system/modules/pelican/wings.nix
+++ system/modules/pelican/wings.nix
@@ -86,7 +86,7 @@ in
     };
 
     environment = mkIf (cfg.impermanence) {
-      persistence."/nix/persist/".directories = [ 
+      persistence."/nix/persist".directories = [ 
         {
           directory = "${cfg.stateDir}/archives";
           mode = "u=rwx,g=,o=";
@@ -106,7 +106,7 @@ in
           group = "pelican";
         }
       ];
-      persistence."/nix/persist/".files = [
+      persistence."/nix/persist".files = [
         {
           file = "${cfg.stateDir}/wings.db";
           parentDirectory = {
