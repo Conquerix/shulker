cfbf020028b23f7bec0597a6480cdc61ae37959b
conquerix <conquerix@shulker.link>
2025-12-04 20:54:50 +0100

    fix permissions of nextcloud statedir

--- system/modules/nextcloud/default.nix
+++ system/modules/nextcloud/default.nix
@@ -62,7 +62,7 @@ in
       "/nix/persist".directories = [
         {
           directory = cfg.stateDir;
-          mode = "u=rwx,g=,o=";
+          mode = "u=rwx,g=rwx,o=";
           user = "nextcloud";
           group = "nextcloud";
         }
