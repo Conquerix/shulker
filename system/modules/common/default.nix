2ff6a5cfb81f59b7fc0009cd3cb7b00aa6a41e57
conquerix <conquerix@shulker.link>
2026-01-25 20:36:40 +0100

    fix recursive imports

--- system/modules/common/default.nix
+++ system/modules/common/default.nix
@@ -1,13 +1,4 @@
 { lib, ... }:
-
-with builtins;
-with lib;
 {
-  imports =
-    let
-      paths = filterAttrs (n: v: v != null && !(hasPrefix "_" n)) (readDir ./.);
-      dirs = filterAttrs (n: v: v == "directory") paths;
-      map' = p: map (x: "${toString ./.}/${x}") (attrNames p);
-    in
-    map' dirs;
+  imports = lib.custom.scanPaths ./.;
 }
