2649c6508a533a2b4cfad42e802439d380cffc75
conquerix <conquerix@shulker.link>
2026-01-31 14:48:27 +0100

    fix backup.nix

--- system/modules/nixos/backup.nix
+++ system/modules/nixos/backup.nix
@@ -34,7 +34,7 @@ in
         ];
       };
     };
-    services.onepassword-secrets.secrets.pelican.hetznerBorgPassword = {
+    services.onepassword-secrets.secrets.hetznerBorgPassword = {
       reference = "op://Shulker/${config.networking.hostName}/Hetzner StorageBox borgbackup password";
       services = [ "borgmatic" ];
     };
