9040b578386d06a66a3e7338fc2d807ce4230bfd
conquerix <conquerix@shulker.link>
2026-02-13 22:43:07 +0100

    add beszel agent, enable on shulker

---
+++ system/modules/nixos/beszel/agent.nix
@@ -0,0 +1,81 @@
+{
+  config,
+  lib,
+  ...
+}:
+
+with lib;
+let
+  cfg = config.shulker.system.modules.beszel.agent;
+in
+{
+  options.shulker.system.modules.beszel.agent = {
+    enable = mkEnableOption "Enable beszel agent service.";
+    impermanence = mkEnableOption "Enable impermanence.";
+    stateDir = mkOption {
+      type = types.str;
+      default = "/var/lib/beszel/agent";
+      description = "State Directory.";
+    };
+    hubEndpoint = mkOption {
+      type = types.str;
+      description = "URL of the Beszel hub.";
+    };
+  };
+
+  config = mkIf cfg.enable {
+
+    networking.firewall = {
+      enable = true;
+      allowedTCPPorts = [ 2022 ];
+      allowedUDPPorts = [ 2022 ];
+    };
+
+    systemd.services.agent.serviceConfig.StateDirectory = mkForce cfg.stateDir;
+
+    services.beszel.agent = {
+      enable = true;
+      smartmon.enable = true;
+      environment = {
+        KEY_FILE = config.services.onepassword-secrets.secrets.beszelAgentKeyFile.path;
+        TOKEN_FILE = config.services.onepassword-secrets.secrets.beszelAgentTokenFile.path;
+        DATA_DIR = cfg.stateDir;
+        FILESYSTEM = "/nix";
+        HUB_URL = cfg.hubEndpoint;
+      };
+    };
+
+    environment = mkIf cfg.impermanence {
+      persistence."/nix/persist".directories = [
+        {
+          directory = "${cfg.stateDir}";
+          mode = "u=rwx,g=,o=";
+          user = "beszel-agent";
+        }
+      ];
+    };
+
+    shulker.system.modules.backup.dirs = [
+      "${cfg.stateDir}/backups"
+      "${cfg.stateDir}/archives"
+    ];
+    services.borgmatic.settings.sqlite_databases = [
+      {
+        name = "beszel-agent-db";
+        path = "${cfg.stateDir}/agent.db";
+      }
+    ];
+
+    services.onepassword-secrets.secrets.beszelAgentKeyFile = {
+      reference = "op://Shulker/${config.networking.hostName}/Beszel Agent Key File";
+      services = [ "beszel-agent" ];
+      owner = "beszel-agent";
+    };
+
+    services.onepassword-secrets.secrets.beszelAgentTokenFile = {
+      reference = "op://Shulker/${config.networking.hostName}/Beszel Agent Token File";
+      services = [ "beszel-agent" ];
+      owner = "beszel-agent";
+    };
+  };
+}
