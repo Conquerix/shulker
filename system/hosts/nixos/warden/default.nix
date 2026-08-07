7a8a75691c3a58e816bf5f6457ab7c8f2fe24765
conquerix <conquerix@shulker.link>
2026-08-07 02:45:43 +0200

    remove todo on warden

--- system/hosts/nixos/warden/default.nix
+++ system/hosts/nixos/warden/default.nix
@@ -69,7 +69,6 @@
         torrent = {
           enable = true;
           downloadDir = "/storage/hdd/torrents";
-          # TODO: set this to warden's actual LAN subnet for web UI access.
           lanNetwork = "10.0.0.0/24";
           webUiPort = 23232;
         };
